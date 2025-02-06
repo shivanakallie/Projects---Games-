ebf4a540b9a9534d8c6068d45bc61ec12e01e1f1
Shivana Kalliecharan <shivanakallie@gmail.com>
2025-02-06 01:52:41 -0500

    Update Dice_Game.py

--- Dice_Game.py
+++ Dice_Game.py
@@ -6,7 +6,6 @@ def roll():
     min_value = 1
     max_value = 6 
     roll = random.randint(min_value, max_value)
-
     return roll 
 
 while True:
@@ -16,9 +15,9 @@ while True:
         if 2 <= players <= 4:
             break
         else:
-         print("Must be between 2 - 4 players.")
+            print("Must be between 2 - 4 players.")
     else: 
-       print("Invalid, try again.")
+        print("Invalid, try again.")
     
 max_score = 50
 player_scores = [0 for _ in range(players)]
@@ -26,31 +25,29 @@ player_scores = [0 for _ in range(players)]
 while max(player_scores) < max_score: 
    
    for player_index in range(players):
-      print("Player", player_index +1, "turn has just started!\n")
+      print("\nPlayer number", player_index + 1, "turn has just started!")
+      print("Your total score is:", player_scores[player_index], "\n")
       current_score = 0
 
       while True:
-         should_roll = input("Would you like to roll (y)?")
+         should_roll = input("Would you like to roll (y)? ")
          if should_roll.lower() != "y":
             break 
          value = roll()
          if value == 1:
-            print("Your rolled a 1! Turn done!")
+            print("You rolled a 1! Turn done!")
             current_score = 0
             break
          else: 
             current_score += value
             print("You rolled a: ", value)
-
             print("Your score is: ", current_score)
-player_scores[player_index] += current_score
-print("Your total score is: ", player_scores[player_index])
 
+      player_scores[player_index] += current_score  # This was misplaced before the loop ends
+      print("Your total score is: ", player_scores[player_index])
 
-    
-    
-            
-   
-
-
+print("Final scores:", player_scores)
+max_score = max(player_scores)
+winning_index = player_scores.index(max_score)
+print("Player number", winning_index + 1, "is the winner with a score of: ", max_score)
 
